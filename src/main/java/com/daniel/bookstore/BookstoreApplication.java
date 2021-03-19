fbcb72685eff99e6d7e9307f5d49eb78a14dac7b
danielVFS <danielv759153@gmail.com>
2021-03-19 10:59:22 -0300

    Book - Repository and seed the database

--- src/main/java/com/daniel/bookstore/BookstoreApplication.java
+++ src/main/java/com/daniel/bookstore/BookstoreApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.bookstore.domain.Author;
+import com.daniel.bookstore.domain.Book;
 import com.daniel.bookstore.domain.Publisher;
 import com.daniel.bookstore.repositories.AuthorRepository;
+import com.daniel.bookstore.repositories.BookRepository;
 import com.daniel.bookstore.repositories.PublisherRepository;
 
 @SpringBootApplication
@@ -19,6 +21,8 @@ public class BookstoreApplication implements CommandLineRunner{
 	private AuthorRepository authorRepository;
 	@Autowired
 	private PublisherRepository publisherRepository;
+	@Autowired
+	private BookRepository bookRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
@@ -40,5 +44,11 @@ public class BookstoreApplication implements CommandLineRunner{
 		
 		publisherRepository.saveAll(Arrays.asList(pb1));
 		
+		///////////////////////////////////////////////////////////
+		
+		Book b1 = new Book(null, "978-0-7334-2635-4", "2018", "Fundamento de C#", 110.0);
+		Book b2 = new Book(null, "952-3-7354-0062-1", "2121", "Criando um blog com ReactJS", 163.0);
+
+		bookRepository.saveAll(Arrays.asList(b1, b2));
 	}
 }
--- src/main/java/com/daniel/bookstore/repositories/BookRepository.java
+++ src/main/java/com/daniel/bookstore/repositories/BookRepository.java
@@ -3,9 +3,9 @@ package com.daniel.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.daniel.bookstore.domain.Publisher;
+import com.daniel.bookstore.domain.Book;
 
 @Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Integer>{
+public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
